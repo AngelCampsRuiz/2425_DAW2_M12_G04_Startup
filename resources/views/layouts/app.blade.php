70b31ed67b9530f06439c0f41279d5993db5f830
Aina Orozco Gonzalez <468417.joan23@fje.edu>
2025-04-07 16:50:45 +0200

    layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Head content -->
+</head>
+<body>
+    @include('partials.header')
+    
+    <main>
+        @yield('content')
+    </main>
+    
+    @include('partials.footer')
+</body>
+</html>
