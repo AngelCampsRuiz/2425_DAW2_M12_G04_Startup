c5a6f3a73d3dfa931058489844413ba49036080f
dbuenaño <deibycesar10@gmail.com>
2025-05-13 17:45:39 +0200

    map2

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,17 @@
     use App\Http\Controllers\ChatController;
     use App\Http\Controllers\ValoracionController;
 
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider and all of them will
+    | be assigned to the "web" middleware group. Make something great!
+    |
+    */
+
     // CONTROLADORES
         // CONTROLADOR HOME
             // CONTROLADOR HOME
